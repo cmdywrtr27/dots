a042e1db56f17401b9f640f49d93e8b63725b3bc
Josh <cmdywrtr27@gmail.com>
2023-02-06 02:49:55 -0700

    startpage

--- .startpage/startpage_4/src/script.js
+++ .startpage/startpage_4/src/script.js
@@ -1,4 +1,4 @@
-const determineGreet = hours => document.getElementById("greeting").innerText = `Good ${hours < 12 ? "Morning." : hours < 18 ? "Afternoon." : "Evening, Josh."}`;
+const determineGreet = hours => document.getElementById("greeting").innerText = `Good ${hours < 12 ? "Morning, Josh." : hours < 18 ? "Afternoon, Josh." : "Evening, Josh."}`;
 
 window.addEventListener('load', (event) => {
     let today = new Date();
